c1da32eef432acf61633ad995b1e2a2774f49d71
Tarik791 <terzotarik@gmail.com>
2024-08-09 00:51:01 +0200

    change.

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -16,9 +17,8 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $allowedIpAddress = '127.0.0.1';
 
-        if ($request->ip() == $allowedIpAddress) {
+        if (Auth::check() && Auth::user()->admin == 1) {
             return $next($request);
         }
 
